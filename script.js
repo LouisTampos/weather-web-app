264986de461d38365a586c472d549fb9aa745ada
Louis Tampos <tamposvictorlouis@gmail.com>
2025-06-08 16:41:06 +0800

    API added

--- script.js
+++ script.js
@@ -16,9 +16,16 @@ PValue = document.getElementById("PValue"),
 Forecast = document.querySelector(".Forecast");
 
 
-/*WEATHER_API_ENDPOINT=;
-WEATHER_DATA_ENDPOINT='' */ 
+WEATHER_API_ENDPOINT= `https://api.weatherapi.com/v1/current.json?key=98917380edfa448f80672505250806&q=`;
+WEATHER_DATA_ENDPOINT=`https://api.weatherapi.com/v1/onecall?.json?key=98917380edfa448f80672505250806&exclude=minutely&units=metric&`;  
 
 function findUserLocation(){
-    alert(1);
+    fetch(WEATHER_API_ENDPOINT+ userLocation.value)
+    .then((response)=>response.json())
+    .then((data) => {
+        if(data.cod!='' && data.cod!=200){
+            alert(data.message);  
+        }
+      console.log(data.coord.lon,data.coord.lat);
+    });
 }
